b76e50e298513210b03547dc0a85c3604fc7e0b1
KevinRichardDev <kevin.richard0@orange.fr>
2024-02-02 05:23:25 +0100

    mealsDisplay()

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 // https://themealdb.com/api/json/v1/1/search.php?s=tomato
 
+const result = document.getElementById("result");
+const form = document.querySelector("form");
 let meals = [];
 
 async function fetchMeals() {
-  await fetch("https://themealdb.com/api/json/v1/1/search.php?s=duck")
+  await fetch("https://themealdb.com/api/json/v1/1/search.php?s=chicken")
     .then((res) => res.json())
-    .then((data) => meals = data.meals);
+    .then((data) => (meals = data.meals));
 
   console.log(meals);
+}
+
+function mealsDisplay() {
+  meals.length = 12;
+
+  result.innerHTML = meals.map(
+    (meal) =>
+      `
+      <h2>${meal.strMeal}</h2>
+    `
+  );
 }
 
 fetchMeals();
